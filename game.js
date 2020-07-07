f6abf33f59e1e3a398356bcdbac19667de67eee3
Andrey Luiz Pereira <62312260+andreylpereira@users.noreply.github.com>
2020-07-07 18:27:13 -0300

    Add files via upload

---
+++ game.js
@@ -0,0 +1,13 @@
+function thegame (number);
+
+  function zeronove(min, max) {
+    min = Math.ceil(0);
+    max = Math.floor(9);
+    return Math.floor(Math.random() * (9 - 0 + 1)) + 0;
+  }
+
+  function zeronoventaenove(min, max) {
+    min = Math.ceil(0);
+    max = Math.floor(99);
+    return Math.floor(Math.random() * (99 - 0 + 1)) + 0;
+  }
